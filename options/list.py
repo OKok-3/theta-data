ac0e09bc10e3cfa6f62d2e22f521e51e9daf9f03
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-12-04 21:56:23 -0500

    refactor: change response type to io.BytesIO for better post-processing
    This gives the user more flexibility when choosing which package to process the raw data in bytes

--- options/list.py
+++ options/list.py
@@ -1,5 +1,6 @@
 """Scrape OHLC data from Theta Data"""
 
+import io
 import os
 
 import httpx
@@ -9,38 +10,37 @@ load_dotenv()
 TT_HOST = os.environ.get("TT_HOST")
 
 
-def get_all_symbols(client: httpx.Client, format: str) -> httpx.Response:
+def get_all_symbols(client: httpx.Client, format: str) -> io.BytesIO:
     """Get all symbols from Theta Data"""
     url = f"{TT_HOST}/option/list/symbols"
     params = {"format": format}
 
-    response = client.get(url, params=params)
-    response.raise_for_status()
+    with client.stream("GET", url, params=params) as response:
+        response.raise_for_status()
+        return io.BytesIO(response.read())
 
-    return response
 
-
-def get_expirations(client: httpx.Client, symbol: str, format: str) -> httpx.Response:
+def get_expirations(client: httpx.Client, symbol: str, format: str) -> io.BytesIO:
     """Get all expirations for a symbol using streaming"""
     url = f"{TT_HOST}/option/list/expirations"
     params = {"format": format, "symbol": symbol}
 
     with client.stream("GET", url, params=params) as response:
         response.raise_for_status()
-        return response
+        return io.BytesIO(response.read())
 
 
-def get_strikes(client: httpx.Client, symbol: str, expiration: str, format: str) -> httpx.Response:
+def get_strikes(client: httpx.Client, symbol: str, expiration: str, format: str) -> io.BytesIO:
     """Get all strikes for a symbol and expiration"""
     url = f"{TT_HOST}/option/list/strikes"
     params = {"format": format, "symbol": symbol, "expiration": expiration}
 
     with client.stream("GET", url, params=params) as response:
         response.raise_for_status()
-        return response
+        return io.BytesIO(response.read())
 
 
-def get_dates(client: httpx.Client, symbol: str, expiration: str, strike: str, right: str, format: str, request_type: str) -> httpx.Response:
+def get_dates(client: httpx.Client, symbol: str, expiration: str, strike: str, right: str, format: str, request_type: str) -> io.BytesIO:
     """Get all dates for a symbol and expiration"""
     url = f"{TT_HOST}/option/list/dates/{request_type}"
     params = {
@@ -53,4 +53,4 @@ def get_dates(client: httpx.Client, symbol: str, expiration: str, strike: str, r
 
     with client.stream("GET", url, params=params) as response:
         response.raise_for_status()
-        return response
+        return io.BytesIO(response.read())
