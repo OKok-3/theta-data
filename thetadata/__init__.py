8dbf1af24d45ed1893391b2db98e5c4c837a2fff
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-12-05 13:39:11 -0500

    feat: implement client class for connecting to the terminal

---
+++ thetadata/__init__.py
@@ -0,0 +1,16 @@
+"""
+UNOFFICIAL Theta Data API Wrapper
+==================================
+
+A Python wrapper for the Theta Data REST API v3, providing a unified interface for accessing financial data across different asset classes.
+
+This package simplifies the process of querying endpoints for options data, with plans to add support for stocks and indices in the future.
+
+Classes
+-------
+# TODO: Add explanations for classes here
+
+Usage
+-----
+# TODO: Add usage example
+"""
---
+++ thetadata/client.py
@@ -0,0 +1,114 @@
+import io
+import os
+
+import httpx
+import pendulum
+import polars as pl
+
+ERROR_CODES_URL = "https://www.dropbox.com/scl/fi/c1zbaq8e45djf5zb8cy26/ErrorCodes.csv?rlkey=ryepbxvk6zmtcwq3n2s3wrf0h&dl=1"
+
+
+class ThetaDataClient:
+    """
+    Client for Theta Data API v3
+
+    Attributes:
+        - `td_terminal_url`: Theta Data Terminal URL (e.g., `http://localhost:25503`)
+        - `error_codes`: Error codes DataFrame from [Theta Data API documentation](https://docs.thetadata.us/Articles/Errors-Exchanges-Conditions/Error-Codes.html)
+        - `cache_dir`: Directory to cache files (default: `.cache`)
+        - `request_timeout`: Request timeout in seconds
+        - `max_retries`: Maximum number of retries for a request
+        - `terminal_queue_size`: Maximum number of concurrent connections to the terminal (default: 16)
+
+    Note, as far as I understand, the terminal handles the concurrent requests by itself, and users are only limited by the queue size configured for the terminal. Hence, the `terminal_queue_size` is the maximum number of concurrent requests that can be made to the terminal.
+    """
+
+    td_terminal_url: str
+    request_timeout: int
+    max_retries: int
+    terminal_queue_size: int
+    cache_dir: str
+
+    client: httpx.Client
+    error_codes: pl.DataFrame
+
+    def __init__(
+        self,
+        td_terminal_url: str,
+        request_timeout: int = 30,
+        max_retries: int = 3,
+        terminal_queue_size: int = 16,
+        cache_dir: str = ".cache",
+    ):
+        self.td_terminal_url = f"{td_terminal_url.rstrip('/')}/v3"
+        self.request_timeout = request_timeout
+        self.max_retries = max_retries
+        self.terminal_queue_size = terminal_queue_size
+        self.cache_dir = cache_dir
+
+        # Initialize the client
+        self.client = httpx.Client(
+            base_url=self.td_terminal_url,
+            timeout=self.request_timeout,
+            http2=True,
+            limits=httpx.Limits(max_connections=self.terminal_queue_size),
+        )
+
+        # Test the connection to the Theta Data API
+        self._test_connection()
+
+        # Initialize necessary files and directories
+        os.makedirs(self.cache_dir, exist_ok=True)
+        self.error_codes = self._get_error_codes()
+
+        self._print_init_info()
+
+    def _get_error_codes(self) -> pl.DataFrame:
+        """Get error codes from Theta Data API documentation"""
+        error_codes_cache_path = f"{self.cache_dir}/error_codes.parquet"
+
+        # Check if cache exists and is fresh (less than 30 days old)
+        if os.path.exists(error_codes_cache_path):
+            mod_time = os.path.getmtime(error_codes_cache_path)
+            if pendulum.from_timestamp(mod_time) > pendulum.now().subtract(days=30):
+                return pl.read_parquet(error_codes_cache_path)
+
+        # Download and cache if missing or stale
+        response = httpx.get(ERROR_CODES_URL, follow_redirects=True)
+        response.raise_for_status()
+
+        error_codes = pl.read_csv(io.StringIO(response.text), truncate_ragged_lines=True)
+        os.makedirs(self.cache_dir, exist_ok=True)
+        error_codes.write_parquet(error_codes_cache_path)
+
+        return error_codes
+
+    def _test_connection(self) -> None:
+        """Test the connection to the Theta Data API"""
+        self.client.get("/").raise_for_status()
+
+    def _print_init_info(self) -> None:
+        """Print initialization information"""
+        info = {
+            "Setting": [
+                "Theta Data Terminal URL",
+                "Request timeout",
+                "Max retries",
+                "Max concurrent connections",
+                "Cache directory",
+            ],
+            "Value": [
+                self.td_terminal_url,
+                f"{self.request_timeout} seconds",
+                str(self.max_retries),
+                str(self.terminal_queue_size),
+                os.path.abspath(self.cache_dir),
+            ],
+        }
+
+        with pl.Config(
+            tbl_hide_dataframe_shape=True,
+            fmt_str_lengths=1000,
+            tbl_formatting="UTF8_FULL",
+        ):
+            print(pl.DataFrame(info))
